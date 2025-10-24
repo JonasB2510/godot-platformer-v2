6a095cc126e5cdb169fa070b6c9256e77e1177bf
JonasB2510 <jonas.buedel@gmail.com>
2025-10-25 00:11:42 +0200

    changed main menu and started level logic

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Button
 @onready var accept_dialog: AcceptDialog = $"../AcceptDialog"
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
 func _on_option_pressed() -> void:
 	accept_dialog.title = "Information"
